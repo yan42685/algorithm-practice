4455bc549f1e0e74e3511d8483a4a758b87d7e8c
alexyan <419725794@qq.com>
2021-01-24 21:15:51 +0800

    refactor

--- src/main/java/design_pattern/behavioural/observer/ListenerRegistry.java
+++ src/main/java/design_pattern/behavioural/observer/ListenerRegistry.java
@@ -2,6 +2,8 @@ package design_pattern.behavioural.observer;
 
 import utils.Asserts;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,18 +23,22 @@ public class ListenerRegistry {
      * 注册 event 和对应的 listener
      */
     public void register(Object listener) {
-
-        Class<?> clazz = listener.getClass();
-        Arrays.stream(clazz.getDeclaredMethods())
-                // 获取对象所有带@Subscribe注解的方法, 如果方法带有注解且参数数量不为 1 则会报错
+        Class<?> listenerClass = listener.getClass();
+        List<Method> annotatedMethods = Arrays.stream(listenerClass.getDeclaredMethods())
                 .filter(method -> method.isAnnotationPresent(Subscribe.class))
-                .forEach(method -> {
-                    Asserts.argumentsLength(1, method);
-                    Class<?> eventClass = method.getParameterTypes()[0];
-                    registry.putIfAbsent(eventClass, new CopyOnWriteArraySet<>());
-                    // 为该类型的event添加listener回调方法
-                    registry.get(eventClass).add((new EventHandler(listener, method)));
-                });
+                .collect(Collectors.toList());
+
+        Class<?> eventClass = null;
+        ArrayList<EventHandler> handlers = new ArrayList<>();
+        for (Method method : annotatedMethods) {
+            // 约定使用@Subscribe注解的方法参数数量为 1 则会报错
+            Asserts.argumentsLength(1, method);
+            eventClass = method.getParameterTypes()[0];
+            registry.putIfAbsent(eventClass, new CopyOnWriteArraySet<>());
+            handlers.add(new EventHandler(listener, method));
+        }
+        // NOTE: CopyOnWriteSet的add方法加了可重入锁，一个个add存在性能问题, 所以用辅助list帮忙一次性add
+        registry.get(eventClass).addAll(handlers);
     }
 
     /**
@@ -40,7 +46,9 @@ public class ListenerRegistry {
      */
     public List<EventHandler> getMatchedHandlers(Object event) {
         return registry.entrySet().stream()
-                .filter(entry -> entry.getKey().equals(event.getClass()))
+                // a.isAssignableFrom(b) 表示 b的对象 instance of a
+                // 即 发送子类型事件, 注册了父类型的 handler也会执行
+                .filter(entry -> entry.getKey().isAssignableFrom(event.getClass()))
                 .flatMap(entry -> entry.getValue().stream())
                 .collect(Collectors.toList());
     }
