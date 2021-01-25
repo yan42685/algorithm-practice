0de6a51811c1217446d8f373db516565e78d62f7
alexyan <419725794@qq.com>
2021-01-25 22:55:19 +0800

    feat: 有限状态自动机

--- src/main/java/design_pattern/behavioural/state/finite_state_machine/test/StateMachineFactory.java
+++ src/main/java/design_pattern/behavioural/state/finite_state_machine/test/StateMachineFactory.java
@@ -3,29 +3,32 @@ package design_pattern.behavioural.state.finite_state_machine.test;
 import design_pattern.behavioural.state.finite_state_machine.State;
 import design_pattern.behavioural.state.finite_state_machine.StateMachine;
 import design_pattern.behavioural.state.finite_state_machine.Transition;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 定义具体 StateMachine 的状态及转移规则
  */
+@Slf4j
 public class StateMachineFactory {
     public static StateMachine create() {
         State greatState = new State(RoleState.GREAT);
         State normalState = new State(RoleState.NORMAL);
         State smallState = new State(RoleState.SMALL);
 
+        // TODO: 用注解实现注册状态
         Transition eatTransition = Transition.builder()
                 .source(normalState)
                 .target(greatState)
                 .eventName(RandomEvent.EAT.getName())
                 // 后期可以重构为发布订阅模式
-                .handler(event -> System.out.printf("触发事件：%s\n", event.getName()))
+                .handler(event -> log.info("触发事件：{}", event.getName()))
                 .build();
 
         Transition starveTransition = Transition.builder()
                 .source(greatState)
                 .target(normalState)
                 .eventName(RandomEvent.STARVE.getName())
-                .handler(event -> System.out.printf("触发事件: %s\n", event.getName()))
+                .handler(event -> log.info("触发事件: {}\n", event.getName()))
                 .build();
 
         normalState.addTransition(eatTransition);
