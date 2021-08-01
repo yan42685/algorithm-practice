48acdd82cbde5c224dc76dd2ac5670968f9a0415
alexyan <419725794@qq.com>
2021-08-01 13:03:17 +0800

    add BeanUtils

---
+++ src/main/java/utils/BeanUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+
+public class BeanUtils {
+    public static <T> T copyProperties(Object source, Class<T> tClass) {
+        try {
+            T target = tClass.newInstance();
+            // 忽略null值
+            BeanUtil.copyProperties(source, target, CopyOptions.create().ignoreNullValue());
+            return target;
+        } catch (Exception e) {
+            throw new RuntimeException("通过反射实例化失败");
+        }
+    }
+}
