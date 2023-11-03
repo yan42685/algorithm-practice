949cd82cad066490fb64884a94dc110ea2fe5b87
alex <419725794@qq.com>
2023-11-03 14:02:40 +0800

    refactor

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -1,13 +1,18 @@
 package test;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.sql.SQLOutput;
 import java.util.*;
 
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println(Float.MAX_VALUE);
+        Movable tank = new Tank(1,2);
+        tank.move();
+        System.out.println(tank.getX());
     }
 
 }
@@ -24,5 +29,23 @@ class Dog extends Animal {
     public synchronized void shout() {
         System.out.println("shout");
 
+    }
+}
+
+@AllArgsConstructor
+@Getter
+class Movable {
+    private int x;
+    private int y;
+    public void move () {
+        System.out.println("origin x: " + x);
+        --x;
+        System.out.println("current x: " + x);
+    }
+}
+
+class Tank extends Movable {
+    public Tank(int x, int y) {
+        super(x, y);
     }
 }
