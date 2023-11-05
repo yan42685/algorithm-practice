23da325078b2017e24ca86ae9e135ad4a7ac5778
alex <419725794@qq.com>
2023-11-05 18:35:07 +0800

    更新枚举实现饿汉单例模式的示例

--- src/main/java/design_pattern/creational/singleton/HungrySingleton2.java
+++ src/main/java/design_pattern/creational/singleton/HungrySingleton2.java
@@ -3,20 +3,29 @@ package design_pattern.creational.singleton;
 /**
  * 饿汉2 枚举实现
  * 用枚举则可以保证线程安全、反射安全、序列化安全
+ * 需要保存状态就用单例模式，不需要就用工具类静态方法
  */
 public enum HungrySingleton2 {
     INSTANCE;
 
-    private Resource instance;
+    private Resource context;
 
     // 默认就是private的，因为无法在外部创建枚举类的实例
-    HungrySingleton2() {
-        instance = new Resource();
+    public void setContext(Resource context) {
+        this.context = context;
     }
 
-    public Resource getInstance() {
-        return instance;
+    public String doSomething() {
+        return "Do something with " + context.toString();
     }
 
+}
+
+class TestHungrySingleton2 {
+    public static void main(String[] args) {
+        HungrySingleton2.INSTANCE.setContext(new Resource());
+        String result = HungrySingleton2.INSTANCE.doSomething();
+        System.out.println(result);
+    }
 }
 
