17f7a58cbb28fdde6d25bb97d21624c20f332b34
alex <419725794@qq.com>
2023-11-05 20:59:16 +0800

    改正AtomicUtils的方法

--- src/main/java/utils/AtomicUtils.java
+++ src/main/java/utils/AtomicUtils.java
@@ -4,19 +4,51 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class AtomicUtils {
+    /**
+     * 尝试加一个值，直到成功为止
+     */
     public static void addInt(AtomicInteger atomicInteger, int delta) {
-        Lambda.repeat(delta, atomicInteger::incrementAndGet);
+        int prevValue;
+        int newValue;
+        do {
+            prevValue = atomicInteger.get();
+            newValue = prevValue + delta;
+        } while (!atomicInteger.compareAndSet(prevValue, newValue));
     }
 
+    /**
+     * 尝试减去一个值，直到成功为止。
+     */
     public static void reduceInt(AtomicInteger atomicInteger, int delta) {
-        Lambda.repeat(delta, atomicInteger::decrementAndGet);
+        int prevValue;
+        int newValue;
+        do {
+            prevValue = atomicInteger.get();
+            newValue = prevValue - delta;
+        } while (!atomicInteger.compareAndSet(prevValue, newValue));
     }
 
+    /**
+     * 尝试加一个值，直到成功为止
+     */
     public static void addLong(AtomicLong atomicLong, long delta) {
-        Lambda.repeat(delta, atomicLong::incrementAndGet);
+        long prevValue;
+        long newValue;
+        do {
+            prevValue = atomicLong.get();
+            newValue = prevValue + delta;
+        } while (!atomicLong.compareAndSet(prevValue, newValue));
     }
 
+    /**
+     * 尝试减去一个值，直到成功为止。
+     */
     public static void reduceLong(AtomicLong atomicLong, long delta) {
-        Lambda.repeat(delta, atomicLong::decrementAndGet);
+        long prevValue;
+        long newValue;
+        do {
+            prevValue = atomicLong.get();
+            newValue = prevValue - delta;
+        } while (!atomicLong.compareAndSet(prevValue, newValue));
     }
 }
